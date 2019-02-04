172ecde89e02f64650c4ed24ec5ec637ab7059e6
Marcos Sarges <dev.marcos.sarges@gmail.com>
2019-02-04 09:08:49 -0200

    error na utilização do sqlite

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppContainer from './router/router';
+import SQLite from 'react-native-sqlite-storage';
 
 export default class App extends Component {
   render() {
